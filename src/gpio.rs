5c0031b02e634cbed10762b837845e26b7122a7d
Arjan Mels <43108771+arjanmels@users.noreply.github.com>
2020-06-14 19:40:56 +0200

    Renamed GPIO fields

--- src/gpio.rs
+++ src/gpio.rs
@@ -419,46 +419,46 @@ macro_rules! impl_input {
 
 impl_input! {
     enable_w1tc, in_, in_data [
-        Gpio0: (0, 0, pin0, func0_out_sel_cfg, func0_in_sel_cfg, sig0_in_sel, func0_in_sel, gpio0, has_pullup_pulldown),
-        Gpio1: (1, 1, pin1, func1_out_sel_cfg, func1_in_sel_cfg, sig1_in_sel, func1_in_sel, u0txd, has_pullup_pulldown),
-        Gpio2: (2, 2, pin2, func2_out_sel_cfg, func2_in_sel_cfg, sig2_in_sel, func2_in_sel, gpio2, has_pullup_pulldown),
-        Gpio3: (3, 3, pin3, func3_out_sel_cfg, func3_in_sel_cfg, sig3_in_sel, func3_in_sel, u0rxd, has_pullup_pulldown),
-        Gpio4: (4, 4, pin4, func4_out_sel_cfg, func4_in_sel_cfg, sig4_in_sel, func4_in_sel, gpio4, has_pullup_pulldown),
-        Gpio5: (5, 5, pin5, func5_out_sel_cfg, func5_in_sel_cfg, sig5_in_sel, func5_in_sel, gpio5, has_pullup_pulldown),
-        Gpio6: (6, 6, pin6, func6_out_sel_cfg, func6_in_sel_cfg, sig6_in_sel, func6_in_sel, sd_clk, has_pullup_pulldown),
-        Gpio7: (7, 7, pin7, func7_out_sel_cfg, func7_in_sel_cfg, sig7_in_sel, func7_in_sel, sd_data0, has_pullup_pulldown),
-        Gpio8: (8, 8, pin8, func8_out_sel_cfg, func8_in_sel_cfg, sig8_in_sel, func8_in_sel, sd_data1, has_pullup_pulldown),
-        Gpio9: (9, 9, pin9, func9_out_sel_cfg, func9_in_sel_cfg, sig9_in_sel, func9_in_sel, sd_data2, has_pullup_pulldown),
-        Gpio10: (10, 10, pin10, func10_out_sel_cfg, func10_in_sel_cfg, sig10_in_sel, func10_in_sel, sd_data3, has_pullup_pulldown),
-        Gpio11: (11, 11, pin11, func11_out_sel_cfg, func11_in_sel_cfg, sig11_in_sel, func11_in_sel, sd_cmd, has_pullup_pulldown),
-        Gpio12: (12, 12, pin12, func12_out_sel_cfg, func12_in_sel_cfg, sig12_in_sel, func12_in_sel, mtdi, has_pullup_pulldown),
-        Gpio13: (13, 13, pin13, func13_out_sel_cfg, func13_in_sel_cfg, sig13_in_sel, func13_in_sel, mtck, has_pullup_pulldown),
-        Gpio14: (14, 14, pin14, func14_out_sel_cfg, func14_in_sel_cfg, sig14_in_sel, func14_in_sel, mtms, has_pullup_pulldown),
-        Gpio15: (15, 15, pin15, func15_out_sel_cfg, func15_in_sel_cfg, sig15_in_sel, func15_in_sel, mtdo, has_pullup_pulldown),
-        Gpio16: (16, 16, pin16, func16_out_sel_cfg, func16_in_sel_cfg, sig16_in_sel, func16_in_sel, gpio16, has_pullup_pulldown),
-        Gpio17: (17, 17, pin17, func17_out_sel_cfg, func17_in_sel_cfg, sig17_in_sel, func17_in_sel, gpio17, has_pullup_pulldown),
-        Gpio18: (18, 18, pin18, func18_out_sel_cfg, func18_in_sel_cfg, sig18_in_sel, func18_in_sel, gpio18, has_pullup_pulldown),
-        Gpio19: (19, 19, pin19, func19_out_sel_cfg, func19_in_sel_cfg, sig19_in_sel, func19_in_sel, gpio19, has_pullup_pulldown),
-        Gpio20: (20, 20, pin20, func20_out_sel_cfg, func20_in_sel_cfg, sig20_in_sel, func20_in_sel, gpio20, has_pullup_pulldown),
-        Gpio21: (21, 21, pin21, func21_out_sel_cfg, func21_in_sel_cfg, sig21_in_sel, func21_in_sel, gpio21, has_pullup_pulldown),
-        Gpio22: (22, 22, pin22, func22_out_sel_cfg, func22_in_sel_cfg, sig22_in_sel, func22_in_sel, gpio22, has_pullup_pulldown),
-        Gpio23: (23, 23, pin23, func23_out_sel_cfg, func23_in_sel_cfg, sig23_in_sel, func23_in_sel, gpio23, has_pullup_pulldown),
-        Gpio25: (25, 25, pin25, func25_out_sel_cfg, func25_in_sel_cfg, sig25_in_sel, func25_in_sel, gpio25, has_pullup_pulldown),
-        Gpio26: (26, 26, pin26, func26_out_sel_cfg, func26_in_sel_cfg, sig26_in_sel, func26_in_sel, gpio26, has_pullup_pulldown),
-        Gpio27: (27, 27, pin27, func27_out_sel_cfg, func27_in_sel_cfg, sig27_in_sel, func27_in_sel, gpio27, has_pullup_pulldown),
+        Gpio0: (0, 0, pin0, func0_out_sel_cfg, func0_in_sel_cfg, sel, in_sel, gpio0, has_pullup_pulldown),
+        Gpio1: (1, 1, pin1, func1_out_sel_cfg, func1_in_sel_cfg, sel, in_sel, u0txd, has_pullup_pulldown),
+        Gpio2: (2, 2, pin2, func2_out_sel_cfg, func2_in_sel_cfg, sel, in_sel, gpio2, has_pullup_pulldown),
+        Gpio3: (3, 3, pin3, func3_out_sel_cfg, func3_in_sel_cfg, sel, in_sel, u0rxd, has_pullup_pulldown),
+        Gpio4: (4, 4, pin4, func4_out_sel_cfg, func4_in_sel_cfg, sel, in_sel, gpio4, has_pullup_pulldown),
+        Gpio5: (5, 5, pin5, func5_out_sel_cfg, func5_in_sel_cfg, sel, in_sel, gpio5, has_pullup_pulldown),
+        Gpio6: (6, 6, pin6, func6_out_sel_cfg, func6_in_sel_cfg, sel, in_sel, sd_clk, has_pullup_pulldown),
+        Gpio7: (7, 7, pin7, func7_out_sel_cfg, func7_in_sel_cfg, sel, in_sel, sd_data0, has_pullup_pulldown),
+        Gpio8: (8, 8, pin8, func8_out_sel_cfg, func8_in_sel_cfg, sel, in_sel, sd_data1, has_pullup_pulldown),
+        Gpio9: (9, 9, pin9, func9_out_sel_cfg, func9_in_sel_cfg, sel, in_sel, sd_data2, has_pullup_pulldown),
+        Gpio10: (10, 10, pin10, func10_out_sel_cfg, func10_in_sel_cfg, sel, in_sel, sd_data3, has_pullup_pulldown),
+        Gpio11: (11, 11, pin11, func11_out_sel_cfg, func11_in_sel_cfg, sel, in_sel, sd_cmd, has_pullup_pulldown),
+        Gpio12: (12, 12, pin12, func12_out_sel_cfg, func12_in_sel_cfg, sel, in_sel, mtdi, has_pullup_pulldown),
+        Gpio13: (13, 13, pin13, func13_out_sel_cfg, func13_in_sel_cfg, sel, in_sel, mtck, has_pullup_pulldown),
+        Gpio14: (14, 14, pin14, func14_out_sel_cfg, func14_in_sel_cfg, sel, in_sel, mtms, has_pullup_pulldown),
+        Gpio15: (15, 15, pin15, func15_out_sel_cfg, func15_in_sel_cfg, sel, in_sel, mtdo, has_pullup_pulldown),
+        Gpio16: (16, 16, pin16, func16_out_sel_cfg, func16_in_sel_cfg, sel, in_sel, gpio16, has_pullup_pulldown),
+        Gpio17: (17, 17, pin17, func17_out_sel_cfg, func17_in_sel_cfg, sel, in_sel, gpio17, has_pullup_pulldown),
+        Gpio18: (18, 18, pin18, func18_out_sel_cfg, func18_in_sel_cfg, sel, in_sel, gpio18, has_pullup_pulldown),
+        Gpio19: (19, 19, pin19, func19_out_sel_cfg, func19_in_sel_cfg, sel, in_sel, gpio19, has_pullup_pulldown),
+        Gpio20: (20, 20, pin20, func20_out_sel_cfg, func20_in_sel_cfg, sel, in_sel, gpio20, has_pullup_pulldown),
+        Gpio21: (21, 21, pin21, func21_out_sel_cfg, func21_in_sel_cfg, sel, in_sel, gpio21, has_pullup_pulldown),
+        Gpio22: (22, 22, pin22, func22_out_sel_cfg, func22_in_sel_cfg, sel, in_sel, gpio22, has_pullup_pulldown),
+        Gpio23: (23, 23, pin23, func23_out_sel_cfg, func23_in_sel_cfg, sel, in_sel, gpio23, has_pullup_pulldown),
+        Gpio25: (25, 25, pin25, func25_out_sel_cfg, func25_in_sel_cfg, sel, in_sel, gpio25, has_pullup_pulldown),
+        Gpio26: (26, 26, pin26, func26_out_sel_cfg, func26_in_sel_cfg, sel, in_sel, gpio26, has_pullup_pulldown),
+        Gpio27: (27, 27, pin27, func27_out_sel_cfg, func27_in_sel_cfg, sel, in_sel, gpio27, has_pullup_pulldown),
     ]
 }
 
 impl_input! {
     enable1_w1tc, in1, in1_data [
-        Gpio32: (32, 0, pin32, func32_out_sel_cfg, func32_in_sel_cfg, sig32_in_sel, func32_in_sel, gpio32, has_pullup_pulldown),
-        Gpio33: (33, 1, pin33, func33_out_sel_cfg, func33_in_sel_cfg, sig33_in_sel, func33_in_sel, gpio33, has_pullup_pulldown),
-        Gpio34: (34, 2, pin34, func34_out_sel_cfg, func34_in_sel_cfg, sig34_in_sel, func34_in_sel, gpio34, no_pullup_pulldown),
-        Gpio35: (35, 3, pin35, func35_out_sel_cfg, func35_in_sel_cfg, sig35_in_sel, func35_in_sel, gpio35, no_pullup_pulldown),
-        Gpio36: (36, 4, pin36, func36_out_sel_cfg, func36_in_sel_cfg, sig36_in_sel, func36_in_sel, gpio36, no_pullup_pulldown),
-        Gpio37: (37, 5, pin37, func37_out_sel_cfg, func37_in_sel_cfg, sig37_in_sel, func37_in_sel, gpio37, no_pullup_pulldown),
-        Gpio38: (38, 6, pin38, func38_out_sel_cfg, func38_in_sel_cfg, sig38_in_sel, func38_in_sel, gpio38, no_pullup_pulldown),
-        Gpio39: (39, 7, pin39, func39_out_sel_cfg, func39_in_sel_cfg, sig39_in_sel, func39_in_sel, gpio39, no_pullup_pulldown),
+        Gpio32: (32, 0, pin32, func32_out_sel_cfg, func32_in_sel_cfg, sel, in_sel, gpio32, has_pullup_pulldown),
+        Gpio33: (33, 1, pin33, func33_out_sel_cfg, func33_in_sel_cfg, sel, in_sel, gpio33, has_pullup_pulldown),
+        Gpio34: (34, 2, pin34, func34_out_sel_cfg, func34_in_sel_cfg, sel, in_sel, gpio34, no_pullup_pulldown),
+        Gpio35: (35, 3, pin35, func35_out_sel_cfg, func35_in_sel_cfg, sel, in_sel, gpio35, no_pullup_pulldown),
+        Gpio36: (36, 4, pin36, func36_out_sel_cfg, func36_in_sel_cfg, sel, in_sel, gpio36, no_pullup_pulldown),
+        Gpio37: (37, 5, pin37, func37_out_sel_cfg, func37_in_sel_cfg, sel, in_sel, gpio37, no_pullup_pulldown),
+        Gpio38: (38, 6, pin38, func38_out_sel_cfg, func38_in_sel_cfg, sel, in_sel, gpio38, no_pullup_pulldown),
+        Gpio39: (39, 7, pin39, func39_out_sel_cfg, func39_in_sel_cfg, sel, in_sel, gpio39, no_pullup_pulldown),
     ]
 }
 
@@ -499,8 +499,8 @@ macro_rules! impl_analog {
                     rtcio.$gpio_reg.modify(|_,w| w.$pad_driver().clear_bit());
 
                     // Disable output
-                    rtcio.rtc_gpio_enable_w1tc.modify(|_,w| {
-                        unsafe { w.rtc_gpio_enable_w1tc().bits(1u32 << $i) }
+                    rtcio.enable_w1tc.modify(|_,w| {
+                        unsafe { w.enable_w1tc().bits(1u32 << $i) }
                     });
 
                     // Disable input
@@ -533,22 +533,22 @@ impl_no_analog! {[
 ]}
 
 impl_analog! {[
-    Gpio36: (0, rtc_io_sensor_pads, rtc_gpio_pin0, rtc_io_sense1_mux_sel, rtc_io_sense1_fun_sel, rtc_gpio_pin0_pad_driver, rtc_io_sense1_fun_ie,),
-    Gpio37: (1, rtc_io_sensor_pads, rtc_gpio_pin1, rtc_io_sense2_mux_sel, rtc_io_sense2_fun_sel, rtc_gpio_pin1_pad_driver, rtc_io_sense2_fun_ie,),
-    Gpio38: (2, rtc_io_sensor_pads, rtc_gpio_pin2, rtc_io_sense3_mux_sel, rtc_io_sense3_fun_sel, rtc_gpio_pin2_pad_driver, rtc_io_sense3_fun_ie,),
-    Gpio39: (3, rtc_io_sensor_pads, rtc_gpio_pin3, rtc_io_sense4_mux_sel, rtc_io_sense4_fun_sel, rtc_gpio_pin3_pad_driver, rtc_io_sense4_fun_ie,),
-    Gpio34: (4, rtc_io_adc_pad, rtc_gpio_pin4, rtc_io_adc1_mux_sel, rtc_io_adc1_fun_sel, rtc_gpio_pin4_pad_driver, rtc_io_adc1_fun_ie,),
-    Gpio35: (5, rtc_io_adc_pad, rtc_gpio_pin5, rtc_io_adc2_mux_sel, rtc_io_adc2_fun_sel, rtc_gpio_pin5_pad_driver, rtc_io_adc1_fun_ie,),
-    Gpio25: (6, rtc_io_pad_dac1, rtc_gpio_pin6, rtc_io_pdac1_mux_sel, rtc_io_pdac1_fun_sel, rtc_gpio_pin6_pad_driver, rtc_io_pdac1_fun_ie, rtc_io_pdac1_rue, rtc_io_pdac1_rde),
-    Gpio26: (7, rtc_io_pad_dac2, rtc_gpio_pin7, rtc_io_pdac2_mux_sel, rtc_io_pdac2_fun_sel, rtc_gpio_pin7_pad_driver, rtc_io_pdac2_fun_ie, rtc_io_pdac2_rue, rtc_io_pdac2_rde),
-    Gpio33: (8, rtc_io_xtal_32k_pad, rtc_gpio_pin8, rtc_io_x32n_mux_sel, rtc_io_x32n_fun_sel, rtc_gpio_pin8_pad_driver, rtc_io_x32n_fun_ie, rtc_io_x32n_rue, rtc_io_x32n_rde),
-    Gpio32: (9, rtc_io_xtal_32k_pad, rtc_gpio_pin9, rtc_io_x32p_mux_sel, rtc_io_x32p_fun_sel, rtc_gpio_pin9_pad_driver, rtc_io_x32p_fun_ie, rtc_io_x32p_rue, rtc_io_x32p_rde),
-    Gpio4:  (10, rtc_io_touch_pad0, rtc_gpio_pin10, rtc_io_touch_pad0_mux_sel, rtc_io_touch_pad0_fun_sel, rtc_gpio_pin10_pad_driver, rtc_io_touch_pad0_fun_ie, rtc_io_touch_pad0_rue, rtc_io_touch_pad0_rde),
-    Gpio0:  (11, rtc_io_touch_pad1, rtc_gpio_pin11, rtc_io_touch_pad1_mux_sel, rtc_io_touch_pad1_fun_sel, rtc_gpio_pin11_pad_driver, rtc_io_touch_pad1_fun_ie, rtc_io_touch_pad1_rue, rtc_io_touch_pad1_rde),
-    Gpio2:  (12, rtc_io_touch_pad2, rtc_gpio_pin12, rtc_io_touch_pad2_mux_sel, rtc_io_touch_pad2_fun_sel, rtc_gpio_pin12_pad_driver, rtc_io_touch_pad2_fun_ie, rtc_io_touch_pad2_rue, rtc_io_touch_pad2_rde),
-    Gpio15: (13, rtc_io_touch_pad3, rtc_gpio_pin13, rtc_io_touch_pad3_mux_sel, rtc_io_touch_pad3_fun_sel, rtc_gpio_pin13_pad_driver, rtc_io_touch_pad3_fun_ie, rtc_io_touch_pad3_rue, rtc_io_touch_pad3_rde),
-    Gpio13: (14, rtc_io_touch_pad4, rtc_gpio_pin14, rtc_io_touch_pad4_mux_sel, rtc_io_touch_pad4_fun_sel, rtc_gpio_pin14_pad_driver, rtc_io_touch_pad4_fun_ie, rtc_io_touch_pad4_rue, rtc_io_touch_pad4_rde),
-    Gpio12: (15, rtc_io_touch_pad5, rtc_gpio_pin15, rtc_io_touch_pad5_mux_sel, rtc_io_touch_pad5_fun_sel, rtc_gpio_pin15_pad_driver, rtc_io_touch_pad5_fun_ie, rtc_io_touch_pad5_rue, rtc_io_touch_pad5_rde),
-    Gpio14: (16, rtc_io_touch_pad6, rtc_gpio_pin16, rtc_io_touch_pad6_mux_sel, rtc_io_touch_pad6_fun_sel, rtc_gpio_pin16_pad_driver, rtc_io_touch_pad6_fun_ie, rtc_io_touch_pad6_rue, rtc_io_touch_pad6_rde),
-    Gpio27: (17, rtc_io_touch_pad7, rtc_gpio_pin17, rtc_io_touch_pad7_mux_sel, rtc_io_touch_pad7_fun_sel, rtc_gpio_pin17_pad_driver, rtc_io_touch_pad7_fun_ie, rtc_io_touch_pad7_rue, rtc_io_touch_pad7_rde),
+    Gpio36: (0, sensor_pads, pin0, sense1_mux_sel, sense1_fun_sel, pad_driver, sense1_fun_ie,),
+    Gpio37: (1, sensor_pads, pin1, sense2_mux_sel, sense2_fun_sel, pad_driver, sense2_fun_ie,),
+    Gpio38: (2, sensor_pads, pin2, sense3_mux_sel, sense3_fun_sel, pad_driver, sense3_fun_ie,),
+    Gpio39: (3, sensor_pads, pin3, sense4_mux_sel, sense4_fun_sel, pad_driver, sense4_fun_ie,),
+    Gpio34: (4, adc_pad, pin4, adc1_mux_sel, adc1_fun_sel, pad_driver, adc1_fun_ie,),
+    Gpio35: (5, adc_pad, pin5, adc2_mux_sel, adc2_fun_sel, pad_driver, adc1_fun_ie,),
+    Gpio25: (6, pad_dac1, pin6, pdac1_mux_sel, pdac1_fun_sel, pad_driver, pdac1_fun_ie, pdac1_rue, pdac1_rde),
+    Gpio26: (7, pad_dac2, pin7, pdac2_mux_sel, pdac2_fun_sel, pad_driver, pdac2_fun_ie, pdac2_rue, pdac2_rde),
+    Gpio33: (8, xtal_32k_pad, pin8, x32n_mux_sel, x32n_fun_sel, pad_driver, x32n_fun_ie, x32n_rue, x32n_rde),
+    Gpio32: (9, xtal_32k_pad, pin9, x32p_mux_sel, x32p_fun_sel, pad_driver, x32p_fun_ie, x32p_rue, x32p_rde),
+    Gpio4:  (10, touch_pad0, pin10, touch_pad0_mux_sel, touch_pad0_fun_sel, pad_driver, touch_pad0_fun_ie, touch_pad0_rue, touch_pad0_rde),
+    Gpio0:  (11, touch_pad1, pin11, touch_pad1_mux_sel, touch_pad1_fun_sel, pad_driver, touch_pad1_fun_ie, touch_pad1_rue, touch_pad1_rde),
+    Gpio2:  (12, touch_pad2, pin12, touch_pad2_mux_sel, touch_pad2_fun_sel, pad_driver, touch_pad2_fun_ie, touch_pad2_rue, touch_pad2_rde),
+    Gpio15: (13, touch_pad3, pin13, touch_pad3_mux_sel, touch_pad3_fun_sel, pad_driver, touch_pad3_fun_ie, touch_pad3_rue, touch_pad3_rde),
+    Gpio13: (14, touch_pad4, pin14, touch_pad4_mux_sel, touch_pad4_fun_sel, pad_driver, touch_pad4_fun_ie, touch_pad4_rue, touch_pad4_rde),
+    Gpio12: (15, touch_pad5, pin15, touch_pad5_mux_sel, touch_pad5_fun_sel, pad_driver, touch_pad5_fun_ie, touch_pad5_rue, touch_pad5_rde),
+    Gpio14: (16, touch_pad6, pin16, touch_pad6_mux_sel, touch_pad6_fun_sel, pad_driver, touch_pad6_fun_ie, touch_pad6_rue, touch_pad6_rde),
+    Gpio27: (17, touch_pad7, pin17, touch_pad7_mux_sel, touch_pad7_fun_sel, pad_driver, touch_pad7_fun_ie, touch_pad7_rue, touch_pad7_rde),
 ]}
